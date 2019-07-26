d572dbdf68a4e55a6d3138b89d4099898c65693f
tranxuanduc1501 <52705263+tranxuanduc1501@users.noreply.github.com>
2019-07-26 18:57:30 +0700

    Add files via upload

---
+++ Bài 2 ngày 23-7-2019 ( Cách ngu).py
@@ -0,0 +1,14 @@
+listnumber= [129,2,182,41,439]
+a= int(input('Enter a number: '))
+if a!=129 and a!=2 and a!=182 and a!=41 and a!=439:
+    print('Number not found in the list')
+elif a==129:
+    print('Number found. Position 1')
+elif a==2:
+    print('Number found. Position 2')
+elif a==182:
+    print('Number found. Position 3')
+elif a==41:
+    print('Number found. Position 4')
+elif a==439:
+    print('Number found. Position 5')
